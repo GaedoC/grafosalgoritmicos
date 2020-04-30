022eff676e39f4be5cd4f86c4986fdccc61ba2f9
mariam6697 <mariam.maldonadom@utem.cl>
2020-04-30 14:01:20 -0400

    Camino mas corto + peso del mismo

--- servicio/grafo.py
+++ servicio/grafo.py
@@ -8,14 +8,14 @@ def buscar_id(L,a):
                 return b
             b+=1
 
-Arista = namedtuple('Arista', 'inicio, final, costo')
+Arista = namedtuple('Arista', 'inicio, final, peso')
 
 class Grafo(object):
     def __init__(self, aristas):
         self.aristas = [self.generar_arista(*arista) for arista in aristas]
 
-    def generar_arista(self, inicio, final, costo=1):
-        return Arista(inicio, final, costo)
+    def generar_arista(self, inicio, final, peso=1):
+        return Arista(inicio, final, peso)
 
     @property
     def vertices(self):
@@ -28,15 +28,21 @@ class Grafo(object):
     def obtener_aristas(self):
         respuesta = []
         for arista in self.aristas:
-            tupla = (arista.inicio, arista.final, arista.costo)
+            tupla = (arista.inicio, arista.final, arista.peso)
             respuesta.append(tupla)
         return respuesta
+
+    def peso_arista(self, inicio, final):
+        for arista in self.aristas:
+            if arista.inicio == inicio and arista.final == final:
+                return arista.peso
+        return 0
 
     @property
     def vecinos(self):
         vecinos = {vertice: set() for vertice in self.vertices}
         for arista in self.aristas:
-            vecinos[arista.inicio].add((arista.final, arista.costo))
+            vecinos[arista.inicio].add((arista.final, arista.peso))
         return vecinos
 
     def dijkstra(self, inicio, final):
@@ -45,25 +51,29 @@ class Grafo(object):
         vertices_anteriores = {
             vertice: None for vertice in self.vertices
         }
+        peso_total = 0
         distancias[inicio] = 0
         vertices = self.vertices.copy()
         while vertices:
             vertice_actual = min(vertices, key=lambda vertice: distancias[vertice])
             if distancias[vertice_actual] == inf:
                 break
-            for vecino, costo in self.vecinos[vertice_actual]:
-                otra_ruta = distancias[vertice_actual] + costo
+            for vecino, peso in self.vecinos[vertice_actual]:
+                otra_ruta = distancias[vertice_actual] + peso
                 if otra_ruta < distancias[vecino]:
                     distancias[vecino] = otra_ruta
                     vertices_anteriores[vecino] = vertice_actual
             vertices.remove(vertice_actual)
         ruta, vertice_actual = deque(), final
         while vertices_anteriores[vertice_actual] is not None:
+            if len(ruta)>0:
+                peso_total = peso_total + self.peso_arista(str(vertice_actual), str(ruta[0]))
             ruta.appendleft(vertice_actual)
             vertice_actual = vertices_anteriores[vertice_actual]
         if ruta:
+            peso_total = peso_total + self.peso_arista(str(vertice_actual), str(ruta[0]))
             ruta.appendleft(vertice_actual)
-        return list(deque(ruta))
+        return (list(deque(ruta)), peso_total)
 
     @property
     def matriz(self):
